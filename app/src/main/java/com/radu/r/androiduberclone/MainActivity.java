fc3e82aa724c812483a6bec849490fa7620dab6b
Radu <radumolea@gmail.com>
2018-05-06 12:15:53 -0400

    starting implementing button functionality, added gradle dependancy

--- app/src/main/java/com/radu/r/androiduberclone/MainActivity.java
+++ app/src/main/java/com/radu/r/androiduberclone/MainActivity.java
@@ -3,11 +3,15 @@ package com.radu.r.androiduberclone;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn,btnRegister;
+
 
     @Override
     protected void attachBaseContext(Context newBase) {
@@ -20,8 +24,11 @@ public class MainActivity extends AppCompatActivity {
         //Before setContentView
         CalligraphyConfig.initDefault(new CalligraphyConfig.Builder().setDefaultFontPath("fonts/Arkhip_font.ttf").setFontAttrId(R.attr.fontPath).build());
 
-
-
         setContentView(R.layout.activity_main);
+
+        //Init View
+        btnRegister = (Button)findViewById(R.id.btnRegister);
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+
     }
 }
